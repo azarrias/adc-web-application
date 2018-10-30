9b59f96761423d9c5f6cb2a1fda00155c3594479
unknown <devandroid@aula.local>
2018-10-30 18:09:19 +0100

    Create button and webview and declare internet permission

--- app/src/main/java/es/pue/android/webapplication/MainActivity.java
+++ app/src/main/java/es/pue/android/webapplication/MainActivity.java
@@ -2,12 +2,28 @@ package es.pue.android.webapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    WebView webv;
+    Button btnLoad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webv = (WebView)findViewById(R.id.webV);
+        btnLoad = (Button)findViewById(R.id.btnLoadURL);
+
+        btnLoad.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        webv.loadUrl("http://www.google.com");
     }
 }
